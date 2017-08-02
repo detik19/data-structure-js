55ea3496c4a5df0ef3ec01bd9f57c6acdbf4a939
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2017-08-02 08:00:58 +0700

    binary search tree test

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -8,11 +8,43 @@ function BinarySearchTree(){
     
     };
     let root =null;
+    let insertNode=function(node,newNode){
+        if(newNode.key<node.key){
+            if(node.left===null){
+                node.left=newNode;
+            }else{
+                insertNode(node.left,newNode);
+            }
+        }else{
+            if(node.right===null){
+                node.right=newNode;
+            }else{
+                insertNode(node.right,newNode);
+            }
+        }
+    };
     this.insert=function(key){
         let newNode=new Node(key);
+        if(root===null){
+            root=newNode;
+        }else{
+            insertNode(root,newNode);
+        }
+    };
+
+    
+    this.getRoot=function(){
+        return root;
     };
     this.search=function(key){};
-    this.inOrderTranverse=function(){};
+    this.inOrderTrnverse=function(){};
+    let inOrderTraverseNode=function(node,callback){
+        if(node!==null){
+            inOrderTraverseNode(node.left, callback);
+            callback(node.key);
+            inOrderTraverseNode(node.right, callback);
+        }
+    };
     this.preOrderTransverse=function(){};
     this.postOrderTransverse=function(){};
     this.min=function(){};
---
+++ binarySearchTree.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var BinarySearchTree= require('./binarySearchTree');
+
+describe('Test insert',function () {
+    it('test insert',function (done) {
+       
+        done();
+    });
+
+});
